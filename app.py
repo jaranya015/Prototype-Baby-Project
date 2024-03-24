c1f0d4398172b4b7bef85c32f83da56eaac91165
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-03-24 21:13:11 +0700

    Change navbar

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template("index.html")
 
+# @app.route('/chat')
+# def chat():
+#     return render_template("chat.py")
+
+# @app.route('/setting')
+# def setting():
+#     return render_template("setting.py")
+
 if __name__ == "__main__":
     app.run(debug=True)
