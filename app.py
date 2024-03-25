1964dfa483f6211d382656eb2bcf2f2466cb6898
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-03-25 18:15:24 +0700

    fail learn.html file

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template("index.html")
 
-# @app.route('/chat')
-# def chat():
-#     return render_template("chat.py")
+@app.route('/chat')
+def chat():
+    return render_template("chat.html")
 
-# @app.route('/setting')
-# def setting():
-#     return render_template("setting.py")
+@app.route('/setting')
+def setting():
+    return render_template("setting.html")
+
+@app.route('/learn')
+def learn():
+    return render_template("learn.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
